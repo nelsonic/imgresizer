4895768c73af226d7e8a6cb60b1c0cdda00e2dec
@nelsonic <nodecoder@gmail.com>
2013-10-24 11:27:36 +0100

    jasmine tests before migrating to mocha

--- imagemagic/test/test.js
+++ imagemagic/test/test.js
@@ -1,8 +1,11 @@
+chai = require('chai')
+assert = chai.assert
+
 describe("IR - Image Resizer", function() {
 
-  // beforeEach(function () {
-  //   IR = require('../../imageresizer.js');
-  // });
+  beforeEach(function () {
+    IR = require('../lib/imageresizer.js');
+  });
 
   xit("should compute the square root of 4 as 2", function() {
     expect(IR.sqrt(4)).toEqual(2);
@@ -15,23 +18,24 @@ describe("IR - Image Resizer", function() {
     it("getImageFormatFromFilename format should be jpg for Wallpaper800x600.jpg", function() {
         var filename = 'http://www.wollemipine.co.uk/acatalog/Wallpaper800x600.jpg';
         format = IR.getImageFormatFromFilename(filename);
-        expect(format).toEqual('jpg');
+        // expect(format).toEqual('jpg'); // jasmine test
+        assert.equal(format, 'jpg');
     });
 
-    it("getImageFormatFromFilename format should be png for rainbow.png", function() {
+    xit("getImageFormatFromFilename format should be png for rainbow.png", function() {
         var filename = 'http://www.wollemipine.co.uk/acatalog/rainbow.png';
         var format = IR.getImageFormatFromFilename(filename);
         expect(format).toEqual('png');
     });
 
-    it("getImageFormatFromFilename should throw error for file without extension e.g: rainbow", function() {
+    xit("getImageFormatFromFilename should throw error for file without extension e.g: rainbow", function() {
         var filename = 'rainbow';
         expect(function(){ 
             IR.getImageFormatFromFilename(filename)
         }).toThrow(new Error("File format none is invalid in rainbow!"));
     });
 
-    it("getImageFormatFromFilename should throw error for file incorrect extension e.g: document.doc", function() {
+    xit("getImageFormatFromFilename should throw error for file incorrect extension e.g: document.doc", function() {
         var filename = 'document.doc';
         expect(function(){ 
             IR.getImageFormatFromFilename(filename)
@@ -39,31 +43,31 @@ describe("IR - Image Resizer", function() {
     });
 
 
-    it("stripDimensionsFromSourceImageName Should strip dimensions from Wallpaper800x600.jpg", function() {
+    xit("stripDimensionsFromSourceImageName Should strip dimensions from Wallpaper800x600.jpg", function() {
         var originalImage = 'http://www.wollemipine.co.uk/acatalog/Wallpaper800x600.jpg';
         var stripped = IR.stripDimensionsFromSourceImageName(originalImage, 'jpg');
         expect(stripped).toEqual('http://www.wollemipine.co.uk/acatalog/Wallpaper.jpg');
     });
 
-    it("stripDimensionsFromSourceImageName Should strip dimensions from mypic3381_x_2468.jpg", function() {
+    xit("stripDimensionsFromSourceImageName Should strip dimensions from mypic3381_x_2468.jpg", function() {
         var originalImage = 'mypic3381_x_2468.jpg';
         var stripped = IR.stripDimensionsFromSourceImageName(originalImage, 'jpg');
         expect(stripped).toEqual('mypic.jpg');
     });
 
-    it("stripDimensionsFromSourceImageName Should NOT strip numbers from andre3000.jpg", function() {
+    xit("stripDimensionsFromSourceImageName Should NOT strip numbers from andre3000.jpg", function() {
         var originalImage = 'andre3000.jpg';
         var stripped = IR.stripDimensionsFromSourceImageName(originalImage, 'jpg');
         expect(stripped).toEqual(originalImage);
     });
 
-    it("stripDimensionsFromSourceImageName Should NOT strip numbers from 2000ad.jpg", function() {
+    xit("stripDimensionsFromSourceImageName Should NOT strip numbers from 2000ad.jpg", function() {
         var originalImage = '2000ad.jpg';
         var stripped = IR.stripDimensionsFromSourceImageName(originalImage, 'jpg');
         expect(stripped).toEqual(originalImage);
     });
 
-    it("stripDimensionsFromSourceImageName Should NOT strip anything from kittens.jpg", function() {
+    xit("stripDimensionsFromSourceImageName Should NOT strip anything from kittens.jpg", function() {
         var originalImage = 'kittens.jpg';
         var stripped = IR.stripDimensionsFromSourceImageName(originalImage, 'jpg');
         expect(stripped).toEqual(originalImage);
